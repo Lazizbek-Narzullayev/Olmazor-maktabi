d5d770049f4f09d16230de96c735bff04bece1fb
Lazizbek-Narzullayev <lazizbeknarzullayev1810@gmail.com>
2026-05-19 01:14:03 +0500

    fix mongodb dns issues

--- node.js/utils/connectDB.js
+++ node.js/utils/connectDB.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import dns from "dns"
 dotenv.config()
 
 export const connectDB = async () => {
     try {
+        dns.setServers(["8.8.8.8", "8.8.4.4"])
         const uri = process.env.MONGO_URI
         await mongoose.connect(uri)
         console.log("✅ MongoDB ga muvaffaqiyatli ulandi!")
